72a34a6cfc67eb64b61fce6d183e369d8277bea0
smccombs-ford <smccombs@ford.com>
2022-02-22 15:08:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-gp84tr-bw91yo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-gp84tr-bw91yo)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+// export class ProductAlertsComponent implements OnInit {
+
+//   @Input() product!: Product;
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
+
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
+}
